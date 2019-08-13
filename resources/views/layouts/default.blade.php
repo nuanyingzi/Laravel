56874d427131cf4b0d616412e2b071aeb3629de3
nuanyingzi <zhongtao1024@gmail.com>
2019-08-13 22:18:34 +0800

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title','Weibo App') --Laravel</title>
+    <title>@yield('title','Weibo App') -- Laravel</title>
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="/">Weibo App</a>
+            <ul class="navbar-nav justify-content-end">
+            <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+            <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+            </ul>
+        </div>
+    </nav>
     @yield('content')
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.default')
 
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            Now you can see <a href="https://learnku.com/courses/laravel-essential-training">Laravel </a> homepage。
+        </p>
+        <p>一切，将从这里开始。</p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+        </p>
+    </div>
 @stop

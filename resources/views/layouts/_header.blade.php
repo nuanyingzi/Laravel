1a2faf86cff2ec32c126e8e270cfba351de82563
nuanyingzi <zhongtao1024@gmail.com>
2019-08-13 23:15:04 +0800

    命名路由

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <a class="navbar-brand" href="/">Weibo App</a>
         <ul class="navbar-nav justify-content-end">
-        <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ route('help') }}">帮助</a></li>
         <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
         </ul>
     </div>

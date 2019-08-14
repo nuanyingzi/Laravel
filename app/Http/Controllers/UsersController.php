19035ee805847e51956ff2ead32a5f1bf0b28b8d
nuanyingzi <zhongtao1024@gmail.com>
2019-08-14 22:55:45 +0800

    “用户显示页面”

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+    //创建用户
     public function create()
     {
         return view('users.create');
+    }
+
+    //展示个人信息
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+    //生成用户头像
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
 }

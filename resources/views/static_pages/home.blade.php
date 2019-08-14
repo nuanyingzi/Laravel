0eafeed344a9279e9db8cb01d11b77fb331c6419
nuanyingzi <zhongtao1024@gmail.com>
2019-08-14 12:10:37 +0800

    用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -8,7 +8,7 @@
         </p>
         <p>一切，将从这里开始。</p>
         <p>
-            <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+            <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
         </p>
     </div>
 @stop

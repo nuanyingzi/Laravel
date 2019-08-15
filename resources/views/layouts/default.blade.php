5b2bc686d340f3789fc87367fdf549f5b25dbcbc
nuanyingzi <zhongtao1024@gmail.com>
2019-08-15 11:08:40 +0800

    完成用户注册功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -12,6 +12,7 @@
 
     <div class="container">
         <div class="offset-md-1 col-md-10">
+        @include('shared._message')
         @yield('content')
         @include('layouts._footer')
         </div>

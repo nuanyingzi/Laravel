a72df3a1e9e8bede1bbb14354cbac0350645504e
nuanyingzi <zhongtao1024@gmail.com>
2019-08-17 22:48:27 +0800

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -66,4 +66,40 @@ class User extends Authenticatable
     {
         return $this->statuses()->orderBy('created_at', 'desc');
     }
+
+    //粉丝关系
+    public function followers()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
+    }
+
+    //用户关注人
+    public function followings()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+    }
+
+    //关注
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    //取消关注
+    public function unfollowing($user_ids)
+    {
+        if(!is_array($user_ids)){
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    //判断是否关注
+    public function isFollowing($user_id)
+    {
+        return $this->followings()->contains($user_id);
+    }
 }
